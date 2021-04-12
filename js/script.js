4c3e98bd4ca2b72bed0831fab459f42ee29d7cc3
bai1493 <77300410+bai1493@users.noreply.github.com>
2021-04-12 01:40:07 -0600

    added comments

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let p,v,r;
 let fillVar =0;
 
 function setup(){
-	var cnv = createCanvas(windowWidth, windowHeight);
+	var cnv = createCanvas(500,800);
 	cnv.parent('container');
 
 	//create video stream w p5 library
@@ -23,7 +23,7 @@ v = color(114,0,247); //violet
 r = color(221,98,138); //rose
 b = color(151,224,255); //blue
 
-
+//help from https://p5js.org/examples/input-rollover.html
 };
 
 function draw(){
@@ -39,136 +39,34 @@ function draw(){
 
 	if(pos){
 		//image(vidCapture, 10, 10);
-		//print(x1 " ", y1 " ");
-var x1 = pos[62][0];
-var y1 = pos[62][1];
 
-var xStartPoint = [200, 400, 200, 400];
-var yStartPoint = [200, 200, 0, 0];
-var fillColours = [p,v,r,b];
+var x1 = pos[62][0]; //x coordinate of nose
+var y1 = pos[62][1]; //y coordinate of nose
+
+var xStartPoint = [200, 400, 200, 400]; //square x coordinates start
+var yStartPoint = [200, 200, 0, 0]; //square y coordinates start
+var fillColours = [p,v,r,b]; //colour variables in array
 
 for (var a in fillColours)
 {
-	fill(fillColours[a]);
-	square(xStartPoint[a], yStartPoint[a], sSize);
+	fill(fillColours[a]); //adding fill colors to squares
+	square(xStartPoint[a], yStartPoint[a], sSize); //make square based on x,y and size coordinates defined at the top
 }
 
-fill(0);
-ellipse(x1,y1,20);
+fill(0); //if start on no square, circle is black
+ellipse(x1,y1,20); //make circle based on nose coordinates
 
-for (var a in xStartPoint)
+for (var a in xStartPoint) //for all squares to keep the colours used based on x and y coordinates of nose
 {
 	if(x1 > xStartPoint[a] && x1 < xStartPoint[a]+sSize && y1 > yStartPoint[a] && y1 < yStartPoint[a]+sSize)
-	{
-		fill(fillColours[a]);
-		fillVar = fillColours[a];
-		ellipse(x1,y1,20);
+	{ //change color of circle based on x and y coordinate of nose
+		fill(fillColours[a]); //add fill to circle
+		fillVar = fillColours[a]; //keep colour of last corresponding square until entering a new one
+		ellipse(x1,y1,20); // make circle
 	}
 }
-fill(fillVar);
-ellipse(x1,y1, 20);
+fill(fillVar); //keeps last colour
+ellipse(x1,y1, 20); //make circle with last colour
+// cTracker.draw(drawCanvas);
 }
 }
-// noStroke();
-// if(s1Over){
-// 	fill(p)
-// 	ellipse(x1,y1, 10);
-// } else if(s2Over){
-// 	fill(v);
-// 	ellipse(x1,y1, 10);
-// } else if(s3Over){
-// 	fill(r);
-// 	ellipse(x1,y1, 10);
-// };
-// let xStartPoint = 100;
-// let xStartPoint2 = 300;
-// let xStartPoint3 = 400;
-// let yStartPoint = 80;
-// let yStartPoint2 = 175;
-	
-// 		fill(p);
-// 		square(xStartPoint,yStartPoint, sSize);
-// 		fill(v);
-// 		square(xStartPoint3,yStartPoint, sSize);
-// 		fill(r);
-// 		square(xStartPoint2, yStartPoint2, sSize);
-
-
-
-// 		//console.log(pos[62][0], pos[62][1]);
-// 			//ellipse would need x,y,d
-// fill(0);
-// ellipse(x1,y1,20);
-
-// if(x1 > xStartPoint && x1 < xStartPoint+sSize && y1 > yStartPoint && y1 < yStartPoint+sSize){
-
-// 			fill(p);
-// 			fillVar = p;
-// 			ellipse(x1,y1, 20);
-// 		}
-
-// if(x1 > xStartPoint3 && x1 < xStartPoint3+sSize && y1 > yStartPoint && y1 < yStartPoint+sSize){
-
-// 			fill(v);
-// 			fillVar = v;
-// 			ellipse(x1,y1, 20);
-// 		}
-
-// if(x1 > xStartPoint2 && x1 < xStartPoint2+sSize && y1 > yStartPoint2 && y1 < yStartPoint2+sSize){
-
-// 			fill(r);
-// 			fillVar = r;
-// 			ellipse(x1,y1, 20);
-// 		}
-// 		fill(fillVar);
-// 		ellipse(x1,y1, 20);
-// 		//line(pos[23][0],pos[23][1],pos[23][0]-30,pos[23][1]-30)
-
-// //console.log(pmouseX, pmouseY);
-		
-// 		//cTracker.draw(drawCanvas);
-// 			}
-
-// }
-// function update(x, y){
-// if(overS1(200, 80, sSize)){
-// 	s1Over = true;
-// 	s2Over = false;
-// 	s3Over = false;
-// } else if(overS2(400, 80, sSize)){
-// 	s1Over = false;
-// 	s2Over = true;
-// 	s3Over = false;
-// } else if(overS3(300, 275, sSize)){
-// 	s1Over = false;
-// 	s2Over = false;
-// 	s3Over = true;
-// } else {
-// 	overS1 = overS2 = overS3 = false;
-// }
-
-// }
-
-// function overS1(x,y,size){
-// 	if(x1 >= x && x1 <= x+size && y1 >= y && <= y+size){
-// 		return = true;
-// 	} else {
-// 		return = false;
-// 	}
-// }
-
-// function overS2(x,y,size){
-// 	if(x1 >= x && x1 <= x+size && y1 >= y && <= y+size){
-// 		return = true;
-// 	} else {
-// 		return = false;
-// 	}
-// }
-
-// function overS3(x,y,size){
-// 	if(x1 >= x && x1 <= x+size && y1 >= y && <= y+size){
-// 		return = true;
-// 	} else {
-// 		return = false;
-// 	}
-// }
